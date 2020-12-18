7b8bc215e704e1a837daa7e7acdfd8fcbdae500b
Luis Cetta <luiscetta@gmail.com>
2020-12-18 00:31:20 -0300

    Creating page home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import './App.css';
 
+import logo from './assets/logo.svg'
+
 function App() {
   return (
     <div className="container">
-      
+      <img src={logo} alt="AirCnC"/>
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre talentos para sua empresa
+        </p>
+
+        <form>
+          <label htmlFor="email">E-MAIL *</label>
+          <input 
+              id="email" 
+              type="email" 
+              placeholder="Seu melhor e-mail"
+          />
+
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
